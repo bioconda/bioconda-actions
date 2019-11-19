c9c607678b9e9d251e2f3a5562910b2510896659
Devon Ryan <dpryan79@gmail.com>
2019-11-19 22:45:19 +0100

    only fail for me

--- bioconda_recipes_issue_responder/src/main.ts
+++ bioconda_recipes_issue_responder/src/main.ts
@@ -1,15 +1,67 @@
 const core = require('@actions/core');
-// const exec = require('@actions/exec');
-// const tc = require('@actions/tool-cache');
+const exec = require('@actions/exec');
+const request = require('request');
 // const io = require('@actions/io');
 // const fs = require('fs');
+
+function sendComment(context, comment) {
+  // url?
+  const issueNumber = context['issue']['number'];
+  const URL = "https://api.github.com/repos/bioconda/bioconda-recipes/" + issueNumber + "/comments";
+  const payLoad = {'body': comment};
+  request.post(URL, {
+    json: {
+      body: comment
+    }});
+}
+
+function mergeInMaster(context) {
+  let branch = '';
+  const options = {listeners: {
+    stdout: (data: Buffer) => {
+      branch += data.toString();
+    },
+  }};
+  exec.exec('git branch | grep \*', options);
+  console.log('The branch is ' + branch.split(" ")[1]);
+  exec.exec('git', ['remote', 'add', 'upstream', 'https://github.com/bioconda/bioconda-recipes']);
+  exec.exec('git', ['checkout', 'master']);
+  exec.exec('git', ['pull', 'upstream', 'master']);
+  exec.exec('git', ['checkout', branch]);
+  exec.exec('git', ['merge', 'master']);
+  console.log('Going to push');
+  exec.exec('git', ['push']);
+  console.log('I pushed!');
+
+  sendComment(context, "OMFG it worked!");
+  // send comment
+}
 
 // This requires that a JOB_CONTEXT environment variable is made with `toJson(github)`
 async function run() {
   console.log(process.env);
   console.log(process.env['JOB_CONTEXT']);
   const jobContext = JSON.parse(<string> process.env['JOB_CONTEXT']);
-  console.log("sha: " + jobContext['sha']);
+  if(jobContext['issue']['pull_request'] !== undefined) {
+    if(jobContext['actor'] != 'dpryan79') {
+      process.exit(0);
+    }
+    const comment = <string> jobContext['comment']['body'];
+    if(comment.includes('@bioconda-bot')) {
+      // Cases are:
+      //   please update
+      //   please merge
+      if(comment.includes('please update')) {
+        mergeInMaster(jobContext);
+      }
+//      } else if comment.includes('please merge') {
+//        mergePR(jobContext);
+//      }
+//    } else if commet.includes('@bioconda/') {
+//      // Check if the user is part of bioconda, otherwise ping
+//      pingBioconda(jobContext);
+    }
+  }
 }
 
 async function runRunner() {
@@ -23,5 +75,3 @@ async function runRunner() {
 }
 
 runRunner();
-console.log('finished');
-process.exit(1);
