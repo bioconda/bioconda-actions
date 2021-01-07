06dd2852a46cb13624ce474bda6376465ffd2ff0
Devon Ryan <devon.ryan@genedata.com>
2021-01-07 14:23:14 +0100

    debug failed setup

--- bioconda_utils_setup_conda/lib/main.js
+++ bioconda_utils_setup_conda/lib/main.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -61,7 +62,9 @@ function run() {
         yield io.mkdirP(home.concat("/miniconda/conda-bld/linux-64"));
         yield io.mkdirP(home.concat("/miniconda/conda-bld/osx-64"));
         yield exec.exec(home.concat("/miniconda/bin/conda"), ["index", home.concat("/miniconda/conda-bld")]);
+        yield exec.exec("ls", ["-l", home.concat("/miniconda/conda-bld")]);
         yield exec.exec(home.concat("/miniconda/bin/conda"), ["config", "--system", "--add", "channels", "file://" + home.concat("/miniconda/conda-bld")]);
+        console.log("finished");
     });
 }
 try {
